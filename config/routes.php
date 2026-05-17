0a28d1868ec3ef6096435954a78cf66ef8485e69
Vitalii Sviatodukh <v.developer15@gmail.com>
2026-05-17 19:39:29 +0300

    fix: renew routes

--- config/routes.php
+++ config/routes.php
@@ -1,20 +1,10 @@
 <?php
 
 use App\Controllers\MainPageController;
-use App\Controllers\DemoController;
-use App\Controllers\TemplateEngineDemoController;
 
 return $routes = [
     '/' => [
         'controller' => MainPageController::class,
-        'action' => 'index'
-    ],
-    '/usage' => [
-        'controller' => DemoController::class,
-        'action' => 'index'
-    ],
-    '/template_engine_example' => [
-        'controller' => TemplateEngineDemoController::class,
         'action' => 'index'
     ],
 ];
