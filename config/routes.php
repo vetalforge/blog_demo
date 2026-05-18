b84d919917c1c293ce60bdd92dfe52735b93b28b
Vitalii Sviatodukh <v.developer15@gmail.com>
2026-05-18 19:21:28 +0300

    feat: add post and category routes

--- config/routes.php
+++ config/routes.php
@@ -1,10 +1,20 @@
 <?php
 
 use App\Controllers\MainPageController;
+use App\Controllers\CategoryController;
+use App\Controllers\PostController;
 
 return $routes = [
     '/' => [
         'controller' => MainPageController::class,
         'action' => 'index'
+    ],
+    '/category/{id}' => [
+        'controller' => CategoryController::class,
+        'action' => 'show'
+    ],
+    '/post/{id}' => [
+        'controller' => PostController::class,
+        'action' => 'show'
     ],
 ];
